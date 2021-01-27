be9008afa1b6ba4a2cd063a0708caff4376c5274
FiqhroDeden <dedhensupatmo@gmail.com>
2021-01-27 12:15:06 +0900

    first commit

---
+++ app/Views/pages/about.php
@@ -0,0 +1,16 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam excepturi doloremque
+                iusto assumenda praesentium iste repellat sed voluptatem consequuntur. Molestiae et nemo m
+                agni quos iure provident nobis commodi blanditiis laboriosam!</p>
+
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
